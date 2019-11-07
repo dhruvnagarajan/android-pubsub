193176281ae3a9f183b2d80bcfaec7f9ac49c32e
Dhruv Nagarajan <emarld2011@gmail.com>
2019-11-07 17:07:15 +0530

    Remove Jackson

--- l3.pubsub/src/main/java/com/dhruvnagarajan/android/pubsub/util/Ext.kt
+++ l3.pubsub/src/main/java/com/dhruvnagarajan/android/pubsub/util/Ext.kt
@@ -1,7 +1,5 @@
 package com.dhruvnagarajan.android.pubsub.util
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
@@ -14,12 +12,6 @@ inline fun <reified T> String.fromJsonInline(): T = Gson().fromJson(this, T::cla
 fun <T> String.fromJson(): T {
     val tt: Type = object : TypeToken<T>() {}.type
     return Gson().fromJson(this, tt)
-}
-
-fun <T> String.readValue(): T {
-    val tt = object : TypeToken<T>() {}.type
-    val mapper = ObjectMapper()
-    return mapper.readValue(this, object : TypeReference<T>() {})
 }
 
 fun Any.toJson(): String = Gson().toJson(this)
